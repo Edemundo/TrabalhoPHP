4c8e4b8ff87c4966aa09e7fe78d1e17b29768741
unknown <edemundo252@gmail.com>
2017-11-23 23:13:50 -0200

    footer arrumado

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,11 @@
 document.write(`<footer>
-    <div class="container">
-        <div class="content has-text-left is-botton">
-            <p>
-             <strong>Venda Bem Caminhoneiro</strong> por Thales Vinicius e Victor Martins</a>.
-            </p>
-        </div>
-    </div>
+<footer class="footer">
+<div class="container">
+  <div class="content has-text-left is-botton">
+    <p>
+      <strong>Venda Bem Caminhoneiro</strong> por Thales Vinicius e Victor Martins</a>.
+    </p>
+  </div>
+</div>
+</footer>
 </footer>`)

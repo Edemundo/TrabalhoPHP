7c7b06337802957eea91b614248972f4aa733c48
Thales Vinicius <thalesburakowski@gmail.com>
2017-11-25 01:58:13 -0200

    login

--- paginas/entrar.php
+++ paginas/entrar.php
@@ -47,6 +47,38 @@
                 </div>
             </form>
         </nav>
+        <?php
+            $email = $_POST["email"];
+            $senha = md5($_POST["senha"]);
+            $query = mysqli_query($conexao, "select (email, senha) from clientes where email = ('{$email}')";
+            $conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+            $queryConsulta = "SELECT * FROM usuarios WHERE ds_usuario = '$email'";
+            $resultado = mysqli_query($conexao, $queryConsulta);
+            $checarResultado = mysqli_num_rows($resultado);
+            if($checarResultado < 1)
+            {
+                ?>
+                <h2 class="subtitle level-item">Usuário não cadastrado!</h2>
+                <?php
+            }
+            else
+            {
+                if ($row = mysqli_fetch_assoc($resultado)) {
+
+                    if ($senha != $row['senha']) 
+                    {
+                    ?>
+                        <h2 class="subtitle level-item">Senha incorreta!</h2>
+                    <?php
+                    }
+                    elseif ($senha == $row['senha']) 
+                    {
+                        $_SESSION['email'] = $row['email'];
+                    }
+                }
+            }
+        ?>
     </article>
 
     <script src="../js/footer.js"></script>
--- php/entrar.php
+++ php/entrar.php
@@ -4,39 +4,25 @@
     $query = mysqli_query($conexao, "select (email, senha) from clientes where email = ('{$email}')";
     $conexao = mysqli_connect('localhost', 'root', '', 'loja');
 
-    if(empty($email) || empty($senha)){
-        header("Location: ../html/index.html?login=campovazio");
-        exit();
+    $queryConsulta = "SELECT * FROM usuarios WHERE ds_usuario = '$email'";
+    $resultado = mysqli_query($conexao, $queryConsulta);
+    $checarResultado = mysqli_num_rows($resultado);
+    if($checarResultado < 1)
+    {
+        // email não existe
     }
     else
-    {	
-        $queryConsulta = "SELECT * FROM usuarios WHERE ds_usuario = '$email'";
-        $resultado = mysqli_query($conexao, $queryConsulta);
-        $checarResultado = mysqli_num_rows($resultado);
-        if($checarResultado < 1)
-        {
-            header("Location: ../html/index.html?login=erro2");
-            exit();
-        }
-        else
-        {
-            if ($row = mysqli_fetch_assoc($resultado)) {
+    {
+        if ($row = mysqli_fetch_assoc($resultado)) {
 
-                if ($senha != $row['senha']) 
-                {
-                    header("Location: ../html/index.html?login=erro1");
-                    exit();
-                }
-                elseif ($senha == $row['senha']) 
-                {
-                    $_SESSION['email'] = $row['email'];
-
-                    header("Location: ../html/index.html?login=sucesso");
-                    exit();
-                }
+            if ($senha != $row['senha']) 
+            {
+            // senha incorreta
+            }
+            elseif ($senha == $row['senha']) 
+            {
+                $_SESSION['email'] = $row['email'];
             }
         }
     }
-header("Location: ../html/index.html?login=erro");
-exit();
 ?>

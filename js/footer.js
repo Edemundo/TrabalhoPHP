d6f6b4ce746d9d1ae6c95908d2f3b848cc40aa3e
Thales Vinicius <thalesburakowski@gmail.com>
2017-11-24 23:10:09 -0200

    fron cadastro caminhao

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,8 @@ document.write(`<footer>
 <div class="container">
   <div class="content has-text-left is-botton">
     <p>
-      <strong>Venda Bem Caminhoneiro</strong> por Thales Vinicius e Victor Martins</a>.
+      <strong>Venda Bem Caminhoneiro</strong> por <a href="https://github.com/thalesburakowski/">Thales Vinícius</a> e
+      <a href="https://github.com/Edemundo">Victor Martins</a>.
     </p>
   </div>
 </div>
